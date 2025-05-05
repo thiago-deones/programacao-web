8263576a151d946dc1d4a4216963ec7713be2377
thiago deones <thiago.deones@gmail.com>
2025-05-05 19:35:42 -0300

    iniciando estudo de javascript

---
+++ aula004/script/script.js
@@ -0,0 +1,4 @@
+console.log("Esta é uma mensagem de lo.");
+console.error("esta é uma mensagem de erro");
+console.warn("Esta é uma mensagem de alerta");
+console.info("Esta é uma menagem de informação");
