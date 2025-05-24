e821fbfa9ce9952d856f4d1ac57516cb8b63de91
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-05-24 18:42:33 -0300

    feat: refactor App component to include FunctionalComponent and ClassComponents; remove unused code

---
+++ battisti/src/components/ClassComponents.jsx
@@ -0,0 +1,12 @@
+class ClassComponents extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Componente de classe</h2>
+            </div>
+        )
+    }
+}
+
+export default ClassComponents;
---
+++ battisti/src/components/FunctionalComponent.jsx
@@ -0,0 +1,15 @@
+//componente filho
+
+
+//criação da função
+function FunctionalComponent() {
+  
+  //retorna um objeto com jsx
+   return (
+        <div>
+            <h2>Testando componetes</h2>
+        </div>
+  );
+}
+
+export default FunctionalComponent;
