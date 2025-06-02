881f3df1864c3eabd256869920341113d4378300
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-06-02 14:34:12 -0300

    feat: update JsxExamples component to display user information and fix JSX syntax in App component

--- reforço-externo/2_jsx/componets/components/JsxExamples.jsx
+++ reforço-externo/2_jsx/componets/components/JsxExamples.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
 const JsxExamples = () => {
+
+
+  const userName = 'João';
+
+  const user = {
+    name: 'João',
+    age: 30
+  };
+
   return (
     <div>
         <h2>Conteúdo que o usuário vai ver</h2>
         {/* O que o usuário não vai ver */}
+        <p>O nome do usúario é: {userName}</p>
+
+        <p>Usúario: {user.name} {user.age}</p>
+
+        <p>{2+2}</p>
     </div>
   )
 }
