0b3740cdaf9d20f7e2f2322e5b1b6d0873e28117
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-06-02 14:50:15 -0300

    feat: update JsxExamples component to change user name and enhance greeting functionality

--- reforço-externo/2_jsx/componets/components/JsxExamples.jsx
+++ reforço-externo/2_jsx/componets/components/JsxExamples.jsx
@@ -6,9 +6,15 @@ const JsxExamples = () => {
   const userName = 'João';
 
   const user = {
-    name: 'João',
+    name: 'Pedro',
     age: 30
   };
+
+  function getGreeting(name) {
+    return `Ola ${name}, seja muito bem-vindo!`;
+  }
+
+  const userIsLoged = true;
 
   return (
     <div>
@@ -19,7 +25,23 @@ const JsxExamples = () => {
         <p>Usúario: {user.name} {user.age}</p>
 
         <p>{2+2}</p>
+
+        <p>{getGreeting(user.name)}</p>
+        <p>{getGreeting("Maria")}</p>
+        <p>{getGreeting(userName)}</p>
+        {/* diferenças do html*/}
+        <div className="alguma-coisa">Este cara</div>
+
+        <div className='teste'>ok</div>
+
+        <button onClick={() => alert('teste')}>Clicke em mim</button>
+
+        <input type="text" placeholder='Digite algo'/>
+
+        {/* RENDERIZAÇÂO CONDICIONAL */}
+        {userIsLoged ? (<p>Caso: está logado</p>) : (<p>Caso: Não está logado</p>)}
     </div>
+
   )
 }
 
