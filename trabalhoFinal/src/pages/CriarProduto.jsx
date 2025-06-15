0c224b482ee1cff535fb80cd31da8770348c68d5
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-06-15 08:31:56 -0300

    feat: implement product creation form with input handling and API integration

--- trabalhoFinal/src/pages/CriarProduto.jsx
+++ trabalhoFinal/src/pages/CriarProduto.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const CriarProduto = () => {
+  const [nome, setNome] = useState('');
+  const [preco, setPreco] = useState('');
+  const navigate = useNavigate();
+
+  //funcao criar novo produto
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://leoproti.com.br:8004/produtos', { nome,
+        preco: parseFloat(preco)
+      });
+      alert('Produto criado com sucesso!');
+      navigate('/'); // voltar para a lista
+    } catch(error) {
+      alert('Erro ao criar produto');
+      console.error(error); 
+    }
+    };
+    
   return (
-    <div>CriarProduto</div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Nome:</label>
+        <input 
+        type="text"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+        required
+         />
+      </div>
+
+      <div>
+        <label>Preço:</label>
+        <input 
+        type="text"
+        step='0.01'
+        value={preco}
+        onChange={(e) => setPreco(e.target.value)}
+        required
+        />
+      </div>
+
+      <button type='submit'>Salvar Produto</button>
+    </form>
   )
 }
 
-export default CriarProduto;
+export default CriarProduto; 
